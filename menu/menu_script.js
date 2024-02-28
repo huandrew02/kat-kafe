b544dc2fa257296e22973af0076b841e8ac75672
Andrew <huandrew321@gmail.com>
2024-02-28 16:59:35 -0500

    added the other pages with css and js

---
+++ menu/menu_script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+  
