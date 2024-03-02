9ee47a5539b73cb4206c4fe291228c90a93f7924
Andrew <huandrew321@gmail.com>
2024-03-02 17:03:40 -0500

    added contact form with email functionality

--- contact/contact_script.js
+++ contact/contact_script.js
@@ -1,8 +1,12 @@
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "100%";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-  }
-  
+  document.getElementById("mySidepanel").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
+
+
+// gmail account used email:355catcafe@gmail.com  pass:catlovers69
+// https://web3forms.com/platforms/html-contact-form
+
