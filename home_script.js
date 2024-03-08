d3be01c3c7cb487e5f2f554d9d651fd70c773169
Andrew <huandrew321@gmail.com>
2024-03-07 22:42:58 -0500

    swapped imgs to use panels

--- home_script.js
+++ home_script.js
@@ -1,10 +1,25 @@
+const panels = document.querySelectorAll('.panel')
+
 function openNav() {
     document.getElementById("mySidepanel").style.width = "100%";
-  }
+}
   
-  function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-  }
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
   
+// for loop
+panels.forEach((panel) => {
+  //console.log(panel);
+  panel.addEventListener('click', () => {
+      removeActiveClasses()
+      panel.classList.add('active')
+  })
+})
 
+function removeActiveClasses() {
+  panels.forEach(panel => { //do not need () b/c 1 argument
+      panel.classList.remove('active')
+  })
+}
 // TODO: make the newsletter work
