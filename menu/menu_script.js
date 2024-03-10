1e9dd6e2983d477e68864a31d6959835ec44b110
Camille Cajas <ccajas001@gmail.com>
2024-03-09 21:33:28 -0500

    add the menu page functionality - utilized day26 vertical slider function project from udemy course

--- menu/menu_script.js
+++ menu/menu_script.js
@@ -2,7 +2,49 @@ function openNav() {
     document.getElementById("mySidepanel").style.width = "100%";
   }
   
-  function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
+
+const menuContainer = document.querySelector('.menu-container')
+const itemTypeTab = document.querySelector('.item-type-tab')
+const menuList = document.querySelector('.menulist')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const slideLength = itemTypeTab.querySelectorAll('div').length
+const itemType = itemTypeTab.querySelectorAll('div')
+
+// NOTE: be sure to change colors if need be here
+
+let activeSlideIndex = 0;
+
+itemType[activeSlideIndex].style.color = `black`;
+itemType[activeSlideIndex].getElementsByTagName('h2')[0].style.fontSize = `30px`
+
+upButton.addEventListener('click', ()=> changeMenuList('up'))
+downButton.addEventListener('click', ()=> changeMenuList('down'))
+
+const changeMenuList = (direction) =>{
+  const containerHeight = menuContainer.clientHeight
+  itemType[activeSlideIndex].style.color = `gray`;
+  itemType[activeSlideIndex].getElementsByTagName('h2')[0].style.fontSize = `25px`
+  if (direction==='down'){
+    activeSlideIndex++;
+    if (activeSlideIndex>slideLength-1){
+      activeSlideIndex=0;
+    }
+    
+    console.log(itemType)
+  }else if (direction==='up'){
+    activeSlideIndex--;
+    if (activeSlideIndex<0){
+      activeSlideIndex=slideLength-1;
+    }
+    
+    console.log(itemType)
   }
-  
+  itemType[activeSlideIndex].style.color = `black`;
+  itemType[activeSlideIndex].getElementsByTagName('h2')[0].style.fontSize = `30px`
+  menuList.style.transform = `translateY(-${activeSlideIndex*containerHeight}px)`
+
+}
