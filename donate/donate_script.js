650e1d066e912586d8bc17fb09652cd6754fe722
JamesSokoll <jsokoll311@gmail.com>
2024-03-11 20:31:15 -0400

    Updated Donate Page

--- donate/donate_script.js
+++ donate/donate_script.js
@@ -1,8 +1,28 @@
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "100%";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-  }
-  
+  document.getElementById("mySidepanel").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
+
+const counters = document.querySelectorAll('.counter')
+
+counters.forEach(counter => {
+  counter.innerText='0'
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute('data-target')
+    const c = +counter.innerText
+
+    const increment = target / 200
+
+    if(c < target){
+      counter.innerText = `${Math.ceil(c + increment)}`
+      setTimeout(updateCounter,1)
+    } else{
+      counter.innerText = target
+    }
+  } 
+  updateCounter()
+})
